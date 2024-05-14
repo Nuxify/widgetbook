ea617eca6f20f876224edd058c698b57d73d11dd
josephgcedeno <josephgcedeno@gmail.com>
2024-05-14 16:37:11 +0800

    feat: empty view placeholder widgetbook

---
+++ widgetbook/lib/indicators/empty_view_placeholder.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:nuxify_widgetbook/indicators/empty_view_placeholder.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(name: 'Default', type: EmptyViewPlaceholder)
+Widget defaultUseCase(BuildContext context) {
+  return Center(
+    child: EmptyViewPlaceholder(
+      height: context.knobs.double.slider(
+        label: 'Empty View PlaceHolder Height',
+        initialValue: 250,
+        max: 400,
+        min: 250,
+      ),
+      graphic: Icon(
+        Icons.broken_image,
+        color: context.knobs.color(label: 'Graphic Color'),
+        size: context.knobs.double.slider(
+          label: 'Graphic size',
+          initialValue: 50,
+          max: 150,
+          min: 50,
+        ),
+      ),
+      label: Text(
+        context.knobs.string(
+          label: 'Label',
+          initialValue: 'Missing record.',
+        ),
+        style: TextStyle(
+          color: context.knobs.color(label: 'Text Color'),
+          fontSize: context.knobs.double.slider(
+            label: 'Label size',
+            initialValue: 12,
+            max: 50,
+            min: 10,
+          ),
+        ),
+      ),
+    ),
+  );
+}
